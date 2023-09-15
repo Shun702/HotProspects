51e5cccd0f655ae066da0124a647dd44c33b2dcb
Shun702 <138392252+Shun702@users.noreply.github.com>
2023-09-15 13:33:51 +0100

    initial commit
    initial code

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Shun Le Yi Mon on 04/09/2023.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    @StateObject var prospects = Prospects()
+    
+    
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
+        .environmentObject(prospects)
+    }
+
+//    func fetchReadings() async {
+//        let fetchTask = Task { () -> String in
+//            let url = URL(string: "https://hws.dev/readings.json")!
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//            let readings = try JSONDecoder().decode([Double].self, from: data)
+//            return "Found \(readings.count) readings"
+//        }
+//    }
+}
+
+//@MainActor class DelayedUpdater: ObservableObject {
+//    var value = 0 {
+//        willSet {
+//            objectWillChange.send()
+//        }
+//    }
+//
+//    init() {
+//        for i in 1...10 {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+//                self.value += 1
+//            }
+//        }
+//    }
+//}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Shun Le Yi Mon on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
